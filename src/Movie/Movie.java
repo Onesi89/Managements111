dce361281e0b9a81fdeb60e37b7409aff0b59259
Onesi89 <kimwpdyd@naver.com>
2022-05-18 15:43:12 +0900

    김제용 2022-05-18 3시

--- src/Movie/Movie.java
+++ src/Movie/Movie.java
@@ -1,5 +1,49 @@
 package Movie;
 
 public class Movie {
+	public String movieName; //영화제목
+	public int visitorNumber; //총 관람객 수
+	public String actors; // 배우들 모임
+	public String plot; //줄거리
+	public int screenNumber; //상영관 숫자
+	public long sales; //매출
+	
+	 //영화 리스트 호출시 생성자
+	Movie(){};
+	
+	
+	//영화 정보 입력할 때 생성자
+	Movie(String movieName,int screenNumber){
+		this.movieName = movieName;
+		this.screenNumber = screenNumber;		
+		this.visitorNumber = 0;
+		this.sales = 0;
+	}
+	
+	
+	public void totalVisitorNumber(int number) {
+		this.visitorNumber += number;
+	}
+	
+	public void totalSales(int number) {
+		this.sales += number;
+	}
+	
+	public void setActor(String actors) {
+		this.actors = actors;
+	}
+	
+	public void setPlot(String plot) {
+		this.plot = plot;
+	}
+	
+	public void movieInfo() {
+		System.out.println("영화 제목 : " + movieName + "\n"+ 
+						   "관람객 수: " + visitorNumber +"\n"+
+						   "영화 배우: " + actors + "\n"+
+						   "줄거리 : " + plot +"\n"+
+						   "상영관 : " + screenNumber);
+	}
+	
 
 }
--- src/SystemManagements/test1.java
+++
@@ -1,8 +0,0 @@
-package SystemManagements;
-
-public class test1 {
-	public static void main(String[] args) {
-		System.out.println("깃허브테스트");
-	}
-
-}
